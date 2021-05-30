05f815dd74d7af2c22b1159150ae22f28fcea96e
Rosyid <rosyidgrobogan@gmail.com>
2021-05-30 16:25:15 +0700

    + initializing project

---
+++ src/main/java/com/rosyidgrobogan/springcreationalpatterns/SpringCreationalPatternsApplication.java
@@ -0,0 +1,13 @@
+package com.rosyidgrobogan.springcreationalpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCreationalPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCreationalPatternsApplication.class, args);
+	}
+
+}
